e625868a1f61ecbddac8272799de7962b884b274
Tristan Slominski <tristan.slominski@gmail.com>
2013-02-04 22:13:00 -0600

    bundle() should be implemented

---
+++ bundle.js
@@ -0,0 +1,12 @@
+/*
+ * bundle.js
+ *
+ * (C) 2013 Crosstalk Systems Inc.
+ */
+"use strict";
+
+var bundle = function bundle () {
+
+}; // bundle
+
+module.exports = bundle;
---
+++ test/bundle.test.js
@@ -0,0 +1,17 @@
+/*
+ * bundle.test.js
+ *
+ * (C) 2013 Crosstalk Systems Inc.
+ */
+"use strict";
+
+var assert = require( 'assert' ),
+    bundle = require( '../bundle' );
+
+describe( 'bundle()', function () {
+
+  it( "should be implemented", function () {
+    assert.fail();
+  });
+
+}); // describe bundle()
